affa244dfe7d04fd61389c451e865eadfb8cb334
jpjahner <jpjahner@gmail.com>
2024-10-29 13:16:21 -0600

    slurm script to zip and unzip large files on beartooth

---
+++ slurms/slurm_zip.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## slurm_zip.sh by JPJ 27 i 23
+## PURPOSE: to zip big files
+## USAGE: sbatch slurm_zip.sh
+
+#SBATCH --job-name=zip
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --account=ysctrout
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=1G
+
+
+gzip /project/ysctrout/hatchsauger/1Saug/demultiplex/sample_fastq/Evens/*.fq
+
