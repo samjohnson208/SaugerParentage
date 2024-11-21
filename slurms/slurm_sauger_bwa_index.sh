5ec025eb04a9912cef45ff26c50c22adc5c3b138
Sam Johnson <samjohnson@sams-mbp-9.uwyo.edu>
2024-11-21 14:45:30 -0700

    adding alginment slurm

--- slurms/slurm_sauger_bwa_index.sh
+++ slurms/slurm_sauger_bwa_index.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-## slurm_sucker_bwa_index.sh by JPJ 29 v 23
+## slurm_sauger_bwa_index.sh by JPJ and SPJ 112124
 ## PURPOSE: bwa index
-## USAGE: sbatch slurm_sucker_bwa_index.sh
+## USAGE: sbatch slurm_sauger_bwa_index.sh
 
 #SBATCH --job-name=index
 #SBATCH --nodes=1
@@ -11,13 +11,13 @@
 #SBATCH --account=ysctrout
 #SBATCH --time=0-12:00:00
 #SBATCH --mem=16G
-#SBATCH -o /project/ysctrout/jjahner/suckers/slurms/std/stdout_bwa_index
-#SBATCH -e /project/ysctrout/jjahner/suckers/slurms/std/stderr_bwa_index
+#SBATCH -o /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stdout_bwa_index
+#SBATCH -e /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stderr_bwa_index
 
 module load arcc/1.0
 module load gcc/12.2.0
 module load bwa/0.7.17
 
-cd /project/ysctrout/reference_genomes/c_latipinnis/
-bwa index -p sucker -a bwtsw sucker_genome.fna
+cd /project/ysctrout/reference_genomes/Perca_flavescens/
+bwa index -p yellowperch -a bwtsw yellowperch_genome.fna
 
