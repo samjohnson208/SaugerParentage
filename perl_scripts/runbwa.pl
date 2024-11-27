96b3b82c774018b1ccb5c05d7af1d1370f24854f
Sam Johnson <samjohnson@Sams-MacBook-Pro-9.local>
2024-11-27 17:04:35 -0500

    updated runbwa.pl and slurm_sauger_runbwa.sh

--- perl_scripts/runbwa.pl
+++ perl_scripts/runbwa.pl
@@ -16,8 +16,8 @@ foreach $fastq (@ARGV){
 	$fastq =~ m/([-\w\.]+)\.fastq$/; ## fragile regexp to catch base name of individuals
 	$id = $1;
 	print "Mapping reads for $id\n";
-	system "bwa aln -n 4 -l 20 -k 2 -t 32 -R 20 -q 10 -f sam_sai/aln_"."$id".".sai /project/ysctrout/reference_genomes/c_latipinnis/sucker $fastq\n";
+	system "bwa aln -n 4 -l 20 -k 2 -t 32 -R 20 -q 10 -f sam_sai/aln_"."$id".".sai /project/ysctrout/reference_genomes/Perca_flavescens/yellowperch $fastq\n";
 	## grep -c 'processor' /proc/cpuinfo  
 	## tells you the number of processors on a system
-	system "bwa samse -n 1 -r \'\@RG\\tID:LOC_"."$id"."\' -f sam_sai/aln_"."$id".".sam /project/ysctrout/reference_genomes/c_latipinnis/sucker sam_sai/aln_"."$id".".sai $fastq\n";
+	system "bwa samse -n 1 -r \'\@RG\\tID:LOC_"."$id"."\' -f sam_sai/aln_"."$id".".sam /project/ysctrout/reference_genomes/Perca_flavescens/yellowperch sam_sai/aln_"."$id".".sai $fastq\n";
 }
