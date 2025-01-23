a162b170f481a6c80664b221b6fce21b6cdea554
Sam Johnson <samjohnson@sams-mbp-9.uwyo.edu>
2025-01-23 15:22:22 -0700

    updated variants script

--- slurms/slurm_sauger_variants.sh
+++ slurms/slurm_sauger_variants.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-## slurm_sucker_variants.sh by JPJ 7 vi 23
+## slurm_sauger_variants.sh by JPJ and SPJ 012325
 ## PURPOSE: to call variants
-## USAGE: sbatch slurm_sucker_variants.sh
+## USAGE: sbatch slurm_sauger_variants.sh
 
 #SBATCH --job-name=variants
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --account=ysctrout
-#SBATCH --time=0-12:00:00
+#SBATCH --time=2-12:00:00
 #SBATCH --mem=1G
-#SBATCH -o /project/ysctrout/jjahner/suckers/slurms/std/stdout_variants
-#SBATCH -e /project/ysctrout/jjahner/suckers/slurms/std/stderr_variants
+#SBATCH -o /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stdout_variants
+#SBATCH -e /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stderr_variants
 
-module load arcc/1.0 gcc/12.2.0 samtools/1.16.1 bcftools/1.16
+module load arcc/1.0 gcc/14.2.0 samtools/1.20 bcftools/1.20
 
-cd /project/ysctrout/jjahner/suckers/sam_sai/
+cd /project/ysctrout/hatchsauger/sam_sai/
 
-bcftools mpileup -a DP,AD,INFO/AD -C 50 -d 250 -f /project/ysctrout/reference_genomes/c_latipinnis/sucker_genome.fna -q 30 -Q 20 -I -b bam_list.txt | bcftools call -v -m -f GQ -O z -o variants_rawfiltered_11jun23.vcf
+bcftools mpileup -a DP,AD,INFO/AD -C 50 -d 250 -f /project/ysctrout/reference_genomes/Perca_flavescens/yellowperch_genome.fna -q 30 -Q 20 -I -b bam_list.txt | bcftools call -v -m -f GQ -O z -o variants_rawfiltered_012325.vcf
 
 
