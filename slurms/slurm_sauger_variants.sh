96144d67ac326a7f9928a377e00ccba7f12c154f
Sam Johnson <samjohnson@sams-mbp-9.uwyo.edu>
2025-02-06 20:33:48 -0700

    modified variants slurm for walleye

--- slurms/slurm_sauger_variants.sh
+++ slurms/slurm_sauger_variants.sh
@@ -16,8 +16,8 @@
 
 module load arcc/1.0 gcc/14.2.0 samtools/1.20 bcftools/1.20
 
-cd /project/ysctrout/hatchsauger/sam_sai/
+cd /project/ysctrout/hatchsauger/sam_sai_svit/
 
-bcftools mpileup -a DP,AD,INFO/AD -C 50 -d 250 -f /project/ysctrout/reference_genomes/Perca_flavescens/yellowperch_genome.fna -q 30 -Q 20 -I -b bam_list.txt | bcftools call -v -m -f GQ -O z -o variants_rawfiltered_012325.vcf
+bcftools mpileup -a DP,AD,INFO/AD -C 50 -d 250 -f /project/ysctrout/reference_genomes/Sander_vitreus/walleye_genome.fna -q 30 -Q 20 -I -b bam_list.txt | bcftools call -v -m -f GQ -O z -o variants_rawfiltered_svit_020625.vcf
 
 
