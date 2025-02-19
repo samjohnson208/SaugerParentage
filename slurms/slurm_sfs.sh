0dbc183623ac5c1dc8aea4689e9ae6df048b02d6
Sam Johnson <samjohnson@sams-mbp-9.uwyo.edu>
2025-02-19 14:18:13 -0700

    commented out

--- slurms/slurm_sfs.sh
+++ slurms/slurm_sfs.sh
@@ -16,12 +16,12 @@
 
 ## load modules, activate and install conda environment, pull in git repo, and change 
 module load arcc/1.0 
-module load gcc/13.2.0 
-module load py-numpy/1.26.1
+module load gcc/14.2.0 
+## module load py-numpy/1.26.1
 module load miniconda3/24.3.0
 
 conda activate easySFS
-conda install -c conda-forge numpy pandas scipy -y
+## conda install -c conda-forge numpy pandas scipy -y
 
 # navigate to easySFS directory and change script permissions
 cd /project/ysctrout/hatchsauger/easySFS
