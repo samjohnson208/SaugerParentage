693d520ef01f6163e755f7da41a5505d903faae4
Sam Johnson <samjohnson@Sams-MacBook-Pro-9.local>
2025-02-25 21:11:14 -0700

    created script for sequoia on full matrix

---
+++ slurms/slurm_sequoia_first.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+## created by SPJ 022525
+## PURPOSE: run sequoia on R on the cluster (on a converted genotype matrix)
+## USAGE: sbatch slurm_sequoia_first.sh
+
+#SBATCH --job-name=sequoia
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --account=ysctrout
+#SBATCH --time=10:00:00
+#SBATCH --mem=10G
+#SBATCH -o /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stdout_sequoia_first
+#SBATCH -e /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stderr_sequoia_first
+
+# cd to directory with your converted genotype matrix
+cd /project/ysctrout/hatchsauger/sam_sai_svit/Sequoia_Inp
+
+# load modules
+module load arcc/1.0 gcc/14.2.0 r/4.4.0
+R
+
+# setwd within R
+setwd("/project/ysctrout/hatchsauger/sam_sai_svit/Sequoia_Inp")
+
+# start generating console output file
+sink("Sequoia_Console_Output_022525.txt")
+
+# unsure if this will work first try. worth a shot.
+install.packages("sequoia")
+library(sequoia)
+library(dplyr)
+
+# read in genotype matrix
+mat <- read.table(file = "variants_maf1_miss9.012_conv", header = FALSE, sep = "\t", 
+                  na.strings = c("NA", "-1"))
+
+# correct row names as sample id's
+mat<- mat[, -1]
+gmmat <- as.matrix(mat)
+ind <- read.table(file = "variants_maf1_miss9.012.indv", header = FALSE)
+str(ind)
+ind <- ind %>% 
+    rename(sample = V1)
+rownames(gmmat) <- ind$sample
+
+# check genotype matrix for samples/loci to be excluded
+check <- CheckGeno(gmmat, quiet = FALSE, Plot = TRUE, Return = "GenoM", Strict = TRUE, DumPrefix = c("F0", "M0"))    
+# There are 2 individuals scored for <5% of SNPs, these WILL BE IGNORED
+# In addition, there are 6 individuals scored for <20% of SNPs,it is advised to treat their assignments with caution
+# After exclusion, There are  1182  individuals and  6468  SNPs.
+
+# generate snp stats and plots for the genotype matrix
+stats <- SnpStats(gmmat, Pedigree = NULL, Duplicates = NULL, Plot = TRUE, quiet = FALSE)
+# Not conditioning on any pedigree
+
+# run sequoia on the genotype matrix
+outfull <- sequoia(GenoM = gmmat, Module = 'ped', MaxSibIter = 42, StrictGenoCheck = TRUE, CalcLLR = TRUE)
+
+# run GetMaybeRel() on the sequoia output
+gmr <- GetMaybeRel(outfull, GenoM = gmmat)
+
+# save each to the correct working directory 
+setwd("/project/ysctrout/hatchsauger/first_sequoia")
+
+save(outfull, file = "Sequoia_OutFull_022525.RData")
+save(gmr, file = "Sequoia_GetMayRel_022525.RData")
+
+# stop output of console to Sequoia_Console_Output_022525.txt
+sink()
+
+#exit R
+quit()
+
+
