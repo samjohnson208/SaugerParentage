c9c3decb320d80c2da0f81fa9b1bdad8f8880544
Sam Johnson <samjohnson@sams-mbp-9.uwyo.edu>
2025-03-05 15:36:31 -0700

    updated for euro perch alignment

--- perl_scripts/runbwa.pl
+++ perl_scripts/runbwa.pl
@@ -9,15 +9,15 @@ unless(@ARGV){
     die "runbwa.pl /fullpath/to/files/*fastq";
 }
 
-unless(-e 'sam_sai_svit'){
-    mkdir 'sam_sai_svit', 0755 or die "Failed to make sam_sai_svit directory";
+unless(-e 'sam_sai_pfluv'){
+    mkdir 'sam_sai_pfluv', 0755 or die "Failed to make sam_sai_pfluv directory";
 }
 foreach $fastq (@ARGV){
 	$fastq =~ m/([-\w\.]+)\.fastq$/; ## fragile regexp to catch base name of individuals
 	$id = $1;
 	print "Mapping reads for $id\n";
-	system "bwa aln -n 4 -l 20 -k 2 -t 32 -R 20 -q 10 -f sam_sai_svit/aln_"."$id".".sai /project/ysctrout/reference_genomes/Sander_vitreus/walleye $fastq\n";
+	system "bwa aln -n 4 -l 20 -k 2 -t 32 -R 20 -q 10 -f sam_sai_pfluv/aln_"."$id".".sai /project/ysctrout/reference_genomes/Perca_fluviatilis/europerch $fastq\n";
 	## grep -c 'processor' /proc/cpuinfo  
 	## tells you the number of processors on a system
-	system "bwa samse -n 1 -r \'\@RG\\tID:LOC_"."$id"."\' -f sam_sai_svit/aln_"."$id".".sam /project/ysctrout/reference_genomes/Sander_vitreus/walleye sam_sai_svit/aln_"."$id".".sai $fastq\n";
+	system "bwa samse -n 1 -r \'\@RG\\tID:LOC_"."$id"."\' -f sam_sai_pfluv/aln_"."$id".".sam /project/ysctrout/reference_genomes/Perca_fluviatilis/europerch sam_sai_pfluv/aln_"."$id".".sai $fastq\n";
 }
