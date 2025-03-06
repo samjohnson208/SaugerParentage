93444969ad6a9623c521eb66dd738cff163532f4
Sam Johnson <samjohnson@sams-mbp-9.uwyo.edu>
2025-03-06 14:55:28 -0700

    updated to include sam to bam and sorting steps

--- perl_scripts/runbwa_mem.pl
+++ perl_scripts/runbwa_mem.pl
@@ -13,11 +13,32 @@ unless(-e 'sam_sai_pflav_mem'){
     mkdir 'sam_sai_pflav_mem', 0755 or die "Failed to make sam_sai_pflav_mem directory";
 }
 
+my $fastq;
 foreach $fastq (@ARGV){
-        $fastq =~ m/([-\w\.]+)\.fastq$/; ## fragile regexp to catch base name of individuals
-        $id = $1;
-        print "Mapping reads for $id\n";
-        system "bwa mem -t 16 /project/ysctrout/reference_genomes/Perca_flavescens/Perca_flavescens.fasta $fastq >  aln_"."$id"."\.sam";
+
+    $fastq =~ m/([A-Z]+[0-9]+_[0-9]+)\.fastq/;
+    #print $fastq;
+    my $id = $1;
+    my $sam = "aln_"."$id"."\.sam";
+    my $bam = "aln_"."$id"."\.bam";
+    my $sorted = "aln_"."$id"."\.sorted\.bam";
+
+    if(-e "/project/ysctrout/hatchsauger/sam_sai_pflav_mem/$sorted"){
+	print "$sorted already exists\n";
+    }else{
+    print "Mapping reads for $id\n";
+
+push @jobarray, "bwa mem -t 16 /project/ysctrout/reference_genomes/Perca_flavescens/Perca_flavescens.fasta $fastq >  aln_$id.sam \n 
+
+	echo \"Converting sam to bam for $id\n\" \n
+
+	samtools view -b -S -o $bam $sam \n
+
+	echo \"Sorting and indexing bam files for $id\n\" \n
+	samtools sort $bam -o $sorted \n
+	samtools index $sorted
+ \n";
+    }
 }
 
 
