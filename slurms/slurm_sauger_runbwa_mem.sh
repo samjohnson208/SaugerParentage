af96bb2918f13ae56c2022617fe1281e6391ee75
Sam Johnson <samjohnson@sams-mbp-9.uwyo.edu>
2025-03-06 13:35:02 -0700

    updated for bwa mem on yellow perch

---
+++ slurms/slurm_sauger_runbwa_mem.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## slurm_sauger_runbwa.sh by JPJ and SPJ 112724
+## PURPOSE: to use runbwa.pl
+## USAGE: sbatch slurm_sauger_runbwa.sh
+
+#SBATCH --job-name=runbwa
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --account=ysctrout
+#SBATCH --time=3-0:00:00
+#SBATCH --mem=5G
+#SBATCH -o /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stdout_runbwa
+#SBATCH -e /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stderr_runbwa
+
+module load arcc/1.0 gcc/14.2.0 bwa/0.7.17
+
+cd /project/ysctrout/hatchsauger/
+
+perl /project/ysctrout/hatchsauger/SaugerParentage/perl_scripts/runbwa_mem.pl /project/ysctrout/hatchsauger/1Saug/rawfastqs/*.fastq
