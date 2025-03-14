e5f33da15cf9951ea61925c00981d2843d66d6c8
Sam Johnson <samjohnson@Sams-MacBook-Pro-9.local>
2025-03-13 23:05:12 -0600

    prepped for pflav mem variant calling

--- slurms/slurm_sauger_variants.sh
+++ slurms/slurm_sauger_variants.sh
@@ -11,13 +11,13 @@
 #SBATCH --account=ysctrout
 #SBATCH --time=3-0:00:00
 #SBATCH --mem=1G
-#SBATCH -o /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stdout_variants_svit_mem
-#SBATCH -e /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stderr_variants_svit_mem
+#SBATCH -o /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stdout_variants_pflav_mem
+#SBATCH -e /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stderr_variants_pflav_mem
 
 module load arcc/1.0 gcc/14.2.0 samtools/1.20 bcftools/1.20
 
-cd /project/ysctrout/hatchsauger/sam_sai_svit_mem/
+cd /project/ysctrout/hatchsauger/sam_sai_pflav_mem/
 
-bcftools mpileup -a DP,AD,INFO/AD -C 50 -d 250 -f /project/ysctrout/reference_genomes/Sander_vitreus/walleye_genome.fna -q 30 -Q 20 -I -b bam_list.txt | bcftools call -v -m -f GQ -O z -o variants_rawfiltered_svit_mem_031325.vcf
+bcftools mpileup -a DP,AD,INFO/AD -C 50 -d 250 -f /project/ysctrout/reference_genomes/Perca_flavescens/yellowperch_genome.fna -q 30 -Q 20 -I -b bam_list.txt | bcftools call -v -m -f GQ -O z -o variants_rawfiltered_pflav_mem_031325.vcf
 
 
