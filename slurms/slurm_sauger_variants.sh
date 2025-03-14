5a66bd37c7282d68cac36d17eadb5f1cc49d3c2f
Sam Johnson <samjohnson@Sams-MacBook-Pro-9.local>
2025-03-13 23:02:20 -0600

    prepped for svit mem variant calling

--- slurms/slurm_sauger_variants.sh
+++ slurms/slurm_sauger_variants.sh
@@ -9,15 +9,15 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --account=ysctrout
-#SBATCH --time=2-12:00:00
+#SBATCH --time=3-0:00:00
 #SBATCH --mem=1G
-#SBATCH -o /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stdout_variants
-#SBATCH -e /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stderr_variants
+#SBATCH -o /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stdout_variants_svit_mem
+#SBATCH -e /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stderr_variants_svit_mem
 
 module load arcc/1.0 gcc/14.2.0 samtools/1.20 bcftools/1.20
 
-cd /project/ysctrout/hatchsauger/sam_sai_svit/
+cd /project/ysctrout/hatchsauger/sam_sai_svit_mem/
 
-bcftools mpileup -a DP,AD,INFO/AD -C 50 -d 250 -f /project/ysctrout/reference_genomes/Sander_vitreus/walleye_genome.fna -q 30 -Q 20 -I -b bam_list.txt | bcftools call -v -m -f GQ -O z -o variants_rawfiltered_svit_020625.vcf
+bcftools mpileup -a DP,AD,INFO/AD -C 50 -d 250 -f /project/ysctrout/reference_genomes/Sander_vitreus/walleye_genome.fna -q 30 -Q 20 -I -b bam_list.txt | bcftools call -v -m -f GQ -O z -o variants_rawfiltered_svit_mem_031325.vcf
 
 
