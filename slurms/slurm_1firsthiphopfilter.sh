430e8f94ee3f75cb39e42f96e0096726df54990f
Sam Johnson <samjohnson@Sams-MBP-9.home.local>
2025-06-15 14:07:28 -0600

    redo slurm 1 with proper naming conventions, prepped slurm 3 for after

--- slurms/slurm_1firsthiphopfilter.sh
+++ slurms/slurm_1firsthiphopfilter.sh
@@ -4,7 +4,7 @@
 ## PURPOSE: filter the raw svit mem vcf to include biallelic sites, no indels, and reads with quality > 20
 ## USAGE: sbatch slurm_1firsthiphopfilter.sh
 
-## note: also used 06/15 to filter yellow perch vcfs for pca
+## note: also used 061525 to filter yellow perch vcfs for pca
 
 #SBATCH --job-name=firstfilter
 #SBATCH --account=ysctrout
@@ -12,7 +12,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=6G
+#SBATCH --mem=10G
 #SBATCH -o /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stdout_pcafilters
 #SBATCH -e /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stderr_pcafilters
 #SBATCH --mail-type=END
@@ -22,13 +22,13 @@ module load arcc/1.0 gcc/14.2.0 vcftools/0.1.17
 cd /project/ysctrout/hatchsauger/sam_sai_pflav_mem
 
 # filter raw vcf for biallelic sites
-vcftools --vcf rehead_variants_rawfiltered_pflav_mem_031325.vcf --min-alleles 2 --max-alleles 2 --out variants_bial --recode 
+vcftools --vcf rehead_variants_rawfiltered_pflav_mem_031325.vcf --min-alleles 2 --max-alleles 2 --out variants_pflav_bial --recode 
 
 # filter bial vcf for indels
-vcftools --vcf variants_bial.recode.vcf --remove-indels  --out variants_bial_noindels --recode
+vcftools --vcf variants_pflav_bial.recode.vcf --remove-indels  --out variants_pflav_bial_noindels --recode
 
 # now biallelic sites, no indels ----->  quality > 20
-vcftools --vcf variants_bial_noindels.recode.vcf --minQ 20 --out variants_pflav_bial_noindels_q20 --recode
+vcftools --vcf variants_pflav_bial_noindels.recode.vcf --minQ 20 --out variants_pflav_bial_noindels_q20 --recode
 
 
 
--- slurms/slurm_3hardhiphopfilter.sh
+++ slurms/slurm_3hardhiphopfilter.sh
@@ -4,26 +4,28 @@
 ## PURPOSE: filter the output from slurm_1firsthiphopfilter.sh according to a hard set of criteria (min/max depth, maf, and miss)
 ## USAGE: sbatch slurm_3hardhiphopfilter.sh
 
+## note: also used 061525 to filter yellow perch vcfs for pca
+
 #SBATCH --job-name=hardfilter
 #SBATCH --account=ysctrout
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=6G
-#SBATCH -o /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stdout_hiphophardfilter
-#SBATCH -e /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stderr_hiphophardfilter
+#SBATCH --mem=10G
+#SBATCH -o /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stdout_pcahardfilter
+#SBATCH -e /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stderr_pcahardfilter
 #SBATCH --mail-type=END
 
 module load arcc/1.0 gcc/14.2.0 vcftools/0.1.17
 
-cd /project/ysctrout/hatchsauger/sam_sai_svit_mem/Sequoia_Inp/maf30_miss95
+cd /project/ysctrout/hatchsauger/sam_sai_pflav_mem
 
 # filter first output file (variants_bial_noindels_q20.recode.vcf) by min and max mean depth per site across all samples.
-vcftools --vcf variants_bial_noindels_q20.recode.vcf  --min-meanDP 8 --max-meanDP 75 --out hard_variants_bial_noindels_q20_mindep8_maxdep75 --recode
+vcftools --vcf variants_pflav_bial_noindels_q20.recode.vcf  --min-meanDP 8 --max-meanDP 75 --out hard_variants_pflav_bial_noindels_q20_mindep8_maxdep75 --recode
 
 # filter the vcf that now includes depth by maf and missing data per site.
-vcftools --vcf hard_variants_bial_noindels_q20_mindep8_maxdep75.recode.vcf  --maf 0.30 --max-missing 0.95 --out hard_variants_bial_noindels_q20_mindep8_maxdep75_maf30_miss95 --recode
+vcftools --vcf hard_variants_pflav_bial_noindels_q20_mindep8_maxdep75.recode.vcf  --maf 0.30 --max-missing 0.95 --out hard_variants_pflav_bial_noindels_q20_mindep8_maxdep75_maf30_miss95 --recode
 
 # create genotype matrix
-vcftools --vcf hard_variants_bial_noindels_q20_mindep8_maxdep75_maf30_miss95.recode.vcf --012 --out hard_variants_bial_noindels_q20_mindep8_maxdep75_maf30_miss95
+vcftools --vcf hard_variants_pflav_bial_noindels_q20_mindep8_maxdep75_maf30_miss95.recode.vcf --012 --out hard_variants_pflav_bial_noindels_q20_mindep8_maxdep75_maf30_miss95
