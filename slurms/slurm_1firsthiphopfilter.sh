019d9abe526ebb3ffc9fba469bbd86755f8bf863
Sam Johnson <samjohnson@sams-mbp-9.uwyo.edu>
2025-07-01 16:39:04 -0600

    changed for pflav mem t3

--- slurms/slurm_1firsthiphopfilter.sh
+++ slurms/slurm_1firsthiphopfilter.sh
@@ -7,6 +7,7 @@
 ## note: also used 061525 to filter yellow perch vcfs for pca
 ## note: also used 062625 to filter yellow perch mem t2 vcfs for randomforest
 ## note: also used 070125 to filter walleye from the rehead_variants_rawfiltered_svit_mem_031325.vcf
+## note: also used 070125 to filter yellow perch from the rehead_variants_rawfiltered_pflav_mem_031325.vcf
 
 #SBATCH --job-name=firstfilter
 #SBATCH --account=ysctrout
@@ -21,16 +22,16 @@
 
 module load arcc/1.0 gcc/14.2.0 vcftools/0.1.17
 
-cd /project/ysctrout/hatchsauger/sam_sai_svit_mem/thin_filtered
+cd /project/ysctrout/hatchsauger/sam_sai_pflav_mem_t3
 
 # filter raw vcf for biallelic sites
-vcftools --vcf rehead_variants_rawfiltered_svit_mem_031325.vcf --min-alleles 2 --max-alleles 2 --out variants_svit_mem_bial --recode 
+vcftools --vcf rehead_variants_rawfiltered_pflav_mem_031325.vcf --min-alleles 2 --max-alleles 2 --out variants_pflav_mem_bial --recode 
 
 # filter bial vcf for indels
-vcftools --vcf variants_svit_mem_bial.recode.vcf --remove-indels  --out variants_svit_mem_bial_noindels --recode
+vcftools --vcf variants_pflav_mem_bial.recode.vcf --remove-indels  --out variants_pflav_mem_bial_noindels --recode
 
 # now biallelic sites, no indels ----->  quality > 20
-vcftools --vcf variants_svit_mem_bial_noindels.recode.vcf --minQ 20 --out variants_svit_mem_bial_noindels_q20 --recode
+vcftools --vcf variants_pflav_mem_bial_noindels.recode.vcf --minQ 20 --out variants_pflav_mem_bial_noindels_q20 --recode
 
 
 
