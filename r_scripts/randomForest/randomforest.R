eee8fe6f922efc0d7d91d318f2a1c420db0b54b4
Sam Johnson <samjohnson@sams-mbp-9.uwyo.edu>
2025-07-02 22:45:55 -0600

    retried randomforest with updated pflav mem vcf from sam_sai_pflav_mem_t3. still unsuccessful.

--- r_scripts/randomForest/randomforest.R
+++ r_scripts/randomForest/randomforest.R
@@ -4,50 +4,65 @@
 # to have a chance to run hiphop on the F2 -> F1 assignment step
 ## USAGE: Rscript randomforest.R
 
+# packages
+install.packages("randomForest")
+library(randomForest)
+library(tidyverse)
+
 # locally:
 setwd("/Users/samjohnson/Desktop/")
 
 # read in genotype matrix
-mat <- read.table(file = "hard_variants_pflav_bial_mem_t2_noindels_q20_mindep3_maxdep75_maf1_miss80.012", header = FALSE, sep = "\t")
+mat <- read.table(file = "hard_variants_pflav_mem_t2_bial_noindels_q20_mindep3_maxdep75_maf1_miss95.012", 
+                  header = FALSE, sep = "\t")
 
 # correct row names as sample id's
 mat<- mat[, -1]
 gmmat <- as.matrix(mat)
-ind <- read.table(file = "hard_variants_pflav_bial_mem_t2_noindels_q20_mindep3_maxdep75_maf1_miss80.012.indv", header = FALSE)
 
+# read in individuals for rownames(gmmat)
+ind <- read.table(file = "hard_variants_pflav_mem_t2_bial_noindels_q20_mindep3_maxdep75_maf1_miss95.012.indv", 
+                  header = FALSE)
 str(ind)
 ind <- ind %>% 
   rename(sample = V1)
-rownames(gmmat) <- ind$sample
+rownames(gmmat) <- ind$sample # add inds to gmmat
 
-pos <- read.table(file = "hard_variants_pflav_bial_mem_t2_noindels_q20_mindep3_maxdep75_maf1_miss80.012.pos", header = FALSE)
-pos <- pos$V1
-colnames(gmmat) <- pos
+# read in positions for colnames(gmmat)
+pos <- read.table(file = "hard_variants_pflav_mem_t2_bial_noindels_q20_mindep3_maxdep75_maf1_miss95.012.pos", 
+                  header = FALSE)
+pos$V3 <- paste(pos$V1, pos$V2, sep = "_") # combine scaffold and position to one entry
+colnames(gmmat) <- pos$V3
 
+# read in list of F0 individuals and their sexes
 f0 <- read.csv(file = "spawned_f0.csv", header = FALSE)
 colnames(f0) <- c("sample", "sex")
 
+# filter gmmat to include only F0's
 gmmat_f0 <- gmmat[rownames(gmmat) %in% f0$sample, ]
+gmmat_f0 <- as.data.frame(gmmat_f0) # convert to df
 
-gmmat_f0 <- as.data.frame(gmmat_f0)
+# need to add this sample column to cbind, going to have to delete after
 gmmat_f0 <- data.frame(sample = NA, gmmat_f0)
 gmmat_f0$sample <- rownames(gmmat_f0)
 
+# can we cbind()? are they in the same order?
+table(f0$sample == rownames(gmmat_f0))
 
+# cbind() and check
 gmmat_f0_sex <- cbind(gmmat_f0, sex = f0$sex)
+table(f0$sex == gmmat_f0_sex$sex) # good!
+
+# bring sex to the front of the df and remove sample so it is not a part of the analysis
 gmmat_f0_sex <- gmmat_f0_sex %>% 
     select(sample, sex, everything())
+gmmat_f0_sex <- gmmat_f0_sex[, -1]
 
-install.packages("randomForest")
-library(randomForest)
-
+# construct randomForest model
 rf_model <- randomForest(as.factor(sex) ~ ., data = gmmat_f0_sex, importance = TRUE)
 print(rf_model)
 importance(rf_model)[,4]
 
-without_sample <- gmmat_f0_sex %>% 
-    select(sex, everything())
-
 importance <- data.frame(importance(rf_model)[,4])
 summary(importance)
-
+# unsuccessful.
