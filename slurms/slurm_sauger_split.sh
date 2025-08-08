3e5ffe0884c4e7f487fe2fa93c361660bdbd98c3
Sam Johnson <samjohnson@Sams-MBP-9.home.local>
2025-08-08 11:08:21 -0600

    ready for split of contam_cleaned all parsed fastq

--- slurms/slurm_sauger_split.sh
+++ slurms/slurm_sauger_split.sh
@@ -9,14 +9,15 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --account=ysctrout
-#SBATCH --time=1-00:00:00
-#SBATCH --mem=1G
+#SBATCH --time=2-00:00:00
+#SBATCH --mem=10G
 #SBATCH -o /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stdout_split
 #SBATCH -e /project/ysctrout/hatchsauger/SaugerParentage/slurms/std/stderr_split
+#SBATCH --mail-user=sjohn208@uwyo.edu
 
 
 module load arcc/1.0 gcc/14.2.0
 
-cd /project/ysctrout/hatchsauger/1Saug/rawreads
+cd /project/ysctrout/hatchsauger/1Saug/contam_cleaned
 
 perl /project/ysctrout/hatchsauger/SaugerParentage/perl_scripts/splitFastq_universal_regex.pl sauger_ids.txt all_parsed.fastq
