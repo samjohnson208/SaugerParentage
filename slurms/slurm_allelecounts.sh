7be201c3702ed340921a4e03d6503a0d40cb9ec0
Sam Johnson <samjohnson@sams-mbp-9.uwyo.edu>
2025-10-10 14:37:36 -0600

    needed a cd

--- slurms/slurm_allelecounts.sh
+++ slurms/slurm_allelecounts.sh
@@ -17,6 +17,8 @@
 
 module load arcc/1.0  gcc/14.2.0 bcftools/1.20
 
+cd /project/ysctrout/hatchsauger/sam_sai_contam_fastp_svit_mem/vcfs/q40/mindep8
+
 {echo -e “chrom\tpos\tn_ref\tn_het\tn_alt\tn_miss”; bcftools query -f ‘%CHROM\t%POS[\t%GT]\n’ rehead_variants_rawfiltered_svit_mem_contam_fastp_bial_noindels_q40_mindep8_maxdep75_maf30_miss95_thin1M.recode.vcf \ | awk ‘{n_ref=0; n_het=0; n_alt=0; n_miss=0; for (i=3; i<=NF; i++) {if ($i==“0/0" || $i==“0|0”) n_ref++; else if ($i==“1/1" || $i==“1|1”) n_alt++; else if ($i==“0/1" || $i==“1/0” || $i==“0|1" || $i==“1|0”) n_het++; else if ($i==“./.” || $i==“.|.“) n_miss++;} print $1, $2, n_ref, n_het, n_alt, n_miss;}’} | column -t > site_counts_thin1M.txt
 	
 	
